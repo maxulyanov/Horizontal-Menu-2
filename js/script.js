72c1da1a7fcf23e412bf36dbd3cab59123106f00
M-Ulyanov <web.ulyanov@gmail.com>
2014-04-09 12:12:00 +0400

    Version 1.1
    Добавлена проверка имеется ли вложенное меню
    Добавлено изображение и некоторые виз. эффекты

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(function(){
+
+	//По умолчанию
 	var defaultSetting = {
 		posTop   : '200px',
 		posLeft  : '100px',
@@ -6,12 +8,20 @@ $(function(){
 	};
 	var li = $('ul#index-menu > li');
 
+	//Присвоим специальный класс
+	$(li).each(function(){
+		if($(this).children('.submenu').length !=0){
+			$(this).addClass('parentmenu');
+		}
+	});
+
+	//Ховер для index-menu
 	$(li).hover(function(){
 		clearTimeout($.data(this,'timer'));
 		if($(this).children('ul.submenu').length != 0){
 			var subli = $(this).find('.submenu > li');
 			$(subli).each(function(i){
-				console.log('i')
+			console.log('i')
 			$(this).stop(1,1).delay((i++) * 50).animate({
 			left : '-60px',
 			opacity : 'show',
@@ -25,16 +35,19 @@ $(function(){
 	    var subli = $(this).find('.submenu > li');	
 		$(subli).stop().animate({
 			opacity : 'hide',
+			padding : '5px',
 		}, 300)
 		.animate({
 			top : defaultSetting.posTop,
 			left : defaultSetting.posLeft,
-		},0)	
+			padding : '0px',
+		},0);
 	    }, this), 200));
 
 
 	});
 
+	//Ховер для подменю
 	$('ul.submenu > li a').hover(function(){
 		$(this).stop().animate({
 			padding : '20px 15px',
